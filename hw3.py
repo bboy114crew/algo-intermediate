3698f77bd7f82b869704f9acfc668a28b4dd0c7a
Thắng Nguỵ <bboy114crew@gmail.com>
2021-07-18 22:17:44 +0700

    Solve Chores

--- hw3.py
+++ hw3.py
@@ -1,18 +1,46 @@
-# Devu, the Dumb Guy
-def devu_the_dumb_guy(n, x, c):
-  c.sort()
-  sum = 0
-  for num in c:
-    sum += num * x
-    if (x > 1):
-      x -= 1
-  print(sum)
+# # Devu, the Dumb Guy
+# def devu_the_dumb_guy(n, x, c):
+#   c.sort()
+#   sum = 0
+#   for num in c:
+#     sum += num * x
+#     if (x > 1):
+#       x -= 1
+#   print(sum)
+
+# # Input
+# nx = list(map(int, input().split()))
+# n = nx[0]
+# x = nx[1]
+# c = list(map(int, input().split()))
+
+# # Run and get output
+# devu_the_dumb_guy(n, x, c)
+
+# chores
+def chores(n, a, b, h):
+  h.sort()
+
+  l = 0
+  r = n - 1
+
+  while (l < b - 1):
+    l +=1
+
+  while (n - r < a):
+    r -= 1
+
+  if (l < r):
+    print(h[r] - h[l])
+  else:
+    print(0)
 
 # Input
-nx = list(map(int, input().split()))
-n = nx[0]
-x = nx[1]
-c = list(map(int, input().split()))
+nab = list(map(int, input().split()))
+n = nab[0]
+a = nab[1]
+b = nab[2]
+h = list(map(int, input().split()))
 
 # Run and get output
-devu_the_dumb_guy(n, x, c)
+chores(n, a, b, h)
