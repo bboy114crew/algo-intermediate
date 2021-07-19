40d7e98424d47984bcdc2106e143ce382e8c7629
Nguy Van Thang <nguy.van.thang@linecorp.com>
2021-07-19 17:26:20 +0700

    Create gukiz

--- hw3.py
+++ hw3.py
@@ -36,70 +36,77 @@
 #     print(0)
 
 # # Input
-# nab = list(map(int, input().split()))
-# n = nab[0]
-# a = nab[1]
-# b = nab[2]
 # h = list(map(int, input().split()))
 
 # # Run and get output
 # chores(n, a, b, h)
 
-# Sort the Array
-def sort_the_array(n, a):
-  is_sorted = True
+# # Sort the Array
+# def sort_the_array(n, a):
+#   is_sorted = True
 
-  for i in range(n - 1):
-    if (a[i] > a[i + 1]):
-      is_sorted = False
-      break
-  if (is_sorted):
-    print("yes")
-    print(1, 1)
-    return
-  else:
-    l = 0
-    r = n - 1
-    if (n == 2):
-      print("yes")
-      print(l + 1, r + 1)
-      return
+#   for i in range(n - 1):
+#     if (a[i] > a[i + 1]):
+#       is_sorted = False
+#       break
+#   if (is_sorted):
+#     print("yes")
+#     print(1, 1)
+#     return
+#   else:
+#     l = 0
+#     r = n - 1
+#     if (n == 2):
+#       print("yes")
+#       print(l + 1, r + 1)
+#       return
 
-    flag = 0
+#     flag = 0
 
-    for i in range(n - 1):
-      if (a[i] > a[i + 1]):
-        l = i
-        break
-    for i in range(n - 1, 0, -1):
-      if (a[i] < a[i - 1]):
-        r = i
-        break
+#     for i in range(n - 1):
+#       if (a[i] > a[i + 1]):
+#         l = i
+#         break
+#     for i in range(n - 1, 0, -1):
+#       if (a[i] < a[i - 1]):
+#         r = i
+#         break
 
-    first = []
-    first = a[0:l:1]
+#     first = []
+#     first = a[0:l:1]
 
-    reversed = a[l:r+1:1]
-    reversed = reversed[::-1]
+#     reversed = a[l:r+1:1]
+#     reversed = reversed[::-1]
 
-    last = []
-    last = a[r + 1:n:1]
+#     last = []
+#     last = a[r + 1:n:1]
 
-    final = [*first, *reversed, *last]
+#     final = [*first, *reversed, *last]
 
-    for i in range(n - 1):
-      if (final[i] > final[i + 1]):
-        flag = 1
-        break
-    if (flag):
-      print("no")
-    else:
-      print("yes")
-      print(l + 1, r + 1)
+#     for i in range(n - 1):
+#       if (final[i] > final[i + 1]):
+#         flag = 1
+#         break
+#     if (flag):
+#       print("no")
+#     else:
+#       print("yes")
+#       print(l + 1, r + 1)
+
+# # Input
+# n = int(input())
+# a = list(map(int, input().split()))
+
+# # Run and get output
+# sort_the_array(n, a)
+
+# GukiZ and Contest
+def gukiz_and_contest(n, a):
+  a.sort()
 
 # Input
 n = int(input())
-a = list(map(int, input().split()))
+a= list(map(int, input().split()))
 
 # Run and get output
-sort_the_array(n, a)
+gukiz_and_contest(n, a)
