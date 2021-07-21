9d9b7104e7a4380cbb84f12ac1d49c16afe9fd92
Nguy Van Thang <nguy.van.thang@linecorp.com>
2021-07-21 17:36:12 +0700

    No issue

--- hw4.py
+++ hw4.py
@@ -16,7 +16,7 @@ import queue
 #       elif (current != '('):
 #         stack.append(current)
 #     result.append(s)
-  
+
 #   for string in range(len(result)):
 #     print(result[string])
 
@@ -29,3 +29,55 @@ import queue
 
 # # Run and get output
 # transform_the_expression(n, expressions)
+
+# Street Parade
+def street_parade(cases):
+  for case in cases:
+    a = case["a"]
+    n = case["n"]
+    stack = []
+    sorted_stack = []
+    n_sorted = sorted(a)
+    car_sorted_index = 0
+    for i in range(n):
+      if (a[i] == n_sorted[car_sorted_index]):
+        sorted_stack.append(a[i])
+
+        if (a[i] + 1 == stack[-1]):
+          car_sorted_index = len(stack)
+          for sub in range(len(stack)):
+            sorted_stack.append(stack.pop())
+        else:
+          car_sorted_index += 1
+      else:
+        stack.append(a[i])
+
+    for j in range(len(stack)):
+      sorted_stack.append(stack.pop())
+
+    can_sort = True
+
+    for index in range(n):
+      if (n_sorted[index] != sorted_stack[index]):
+        can_sort = False
+        break
+    if (can_sort):
+      print('yes')
+    else:
+      print('no')
+# Input
+cases = []
+n = 1
+while (True):
+  n = int(input())
+  if (n == 0):
+    break
+  else:
+    case = {}
+    a = list(map(int, input().split()))
+    case["n"] = n
+    case["a"] = a
+    cases.append(case)
+
+# Run and get output
+street_parade(cases)
