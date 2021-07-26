06cd759944b4d8ffdac46eeb80c4bb5f6501d9cd
Nguy Van Thang <nguy.van.thang@linecorp.com>
2021-07-26 17:45:20 +0700

    Push code

--- hw5.py
+++ hw5.py
@@ -1,3 +1,5 @@
+import queue
+
 # Breadth First Search: Shortest Reach
 q = int(input())
 
@@ -16,12 +18,23 @@ def bfs_shortest_reach(q):
       edge = list(map(int, input().split()))
       adj_list[edge[0]].append(edge[1])
       adj_list[edge[1]].append(edge[0])
-    
+
+    q = queue.Queue()
     start = int(input())
-
+    visited[start] = True
+    q.put(start)
     dist[start] = 0
+    while not q.empty():
+      u = q.get()
+      for v in adj_list[u]:
+        if (not visited[v]):
+          visited[v] = True
+          q.put(v)
+          dist[v] = dist[u] + 6
+    result = ''
+    for j in range(1, len(dist)):
+      if (j != start):
+        result = result + str(dist[j]) + " "
+    print(result)
 
-    
-
-
-
+bfs_shortest_reach(q)
