ea854b9e1eb71f327c4b3db948a2d7ccae0cde30
Thắng Nguỵ <bboy114crew@gmail.com>
2021-07-28 00:05:18 +0700

    Solve Bishu and his Girlfriend

--- hw6.py
+++ hw6.py
@@ -1,51 +1,47 @@
 # Bishu and his Girlfriend
-
-def bishu_and_his_girlfriend(s):
-  global vistited, dist, graph
-
-  vistited[s] = True
-  for v in graph[s]:
-    if not vistited[s]:
-      dist[v] = dist[s] + 1
-      bishu_and_his_girlfriend(v)
-
-
-
+def bishu_and_his_girlfriend(s, visited, dist, graph):
+  visited[s] = True
+  stack = []
+  stack.append(s)
+  while (len(stack) != 0):
+    u = stack.pop()
+    for v in graph[u]:
+      if not visited[v]:
+        visited[v] = True
+        dist[v] = dist[u] + 1
+        stack.append(v)
 
 # Input
 n = int(input())
 
-dist = [-1 for index in range(n)]
-dist[0] = 0
+dist = [-1 for index in range(n + 1)]
+dist[1] = 0
 
-min_distance = n
-min_id = n - 1
+min_distance = n + 1
+min_id = n
 
-graph = [[] for index in range(n)]
+graph = [[] for index in range(n + 1)]
 for _ in range(n - 1):
   u, v = list(map(int, input().split()))
-  graph[u - 1].append(v - 1)
-  graph[v - 1].append(u - 1)
+  graph[u].append(v)
+  graph[v].append(u)
 
 q = int(input())
-vistited = [False for index in range(n)]
+visited = [False for index in range(n + 1)]
 city_of_girl = []
 
 for _ in range(q):
   city = int(input())
-  city_of_girl.append(city - 1)
+  city_of_girl.append(city)
 
 # Run and output
-bishu_and_his_girlfriend(0)
+bishu_and_his_girlfriend(1, visited, dist, graph)
 
-min_distance = city_of_girl[0]
-min_id = 0
-
-print(dist)
-
-for i in range(1, len(city_of_girl)):
-  if (dist[city_of_girl[i]] < min_distance or ((dist[city_of_girl[i]] == min_distance) and (min_id > i))):
+for i in range(len(city_of_girl)):
+  if (
+    dist[city_of_girl[i]] < min_distance or 
+    ((dist[city_of_girl[i]] == min_distance) and (min_id > city_of_girl[i]))):
     min_distance = dist[city_of_girl[i]]
-    min_id = i
+    min_id = city_of_girl[i]
 
-print(min_id + 1)
+print(min_id)
