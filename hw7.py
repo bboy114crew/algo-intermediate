b1874d89734fcdbf66170a259157c35f2b3a505c
Nguy Van Thang <nguy.van.thang@linecorp.com>
2021-08-02 18:00:22 +0700

    Qheap 1

--- hw7.py
+++ hw7.py
@@ -8,27 +8,55 @@ class PQEntry:
   def __eq__(self, other):
     return self.value == other.value
 
-# Monk and Multiplication
-def monk_and_multiplication(n, a):
-  h = []
-  for i in range(n):
-    heapq.heappush(h, PQEntry(a[i]))
-    if (i < 2):
-      print(-1)
-    else:
-      max_1 = heapq.heappop(h).value
-      max_2 = heapq.heappop(h).value
-      max_3 = heapq.heappop(h).value
+# # Monk and Multiplication
+# def monk_and_multiplication(n, a):
+#   h = []
+#   for i in range(n):
+#     heapq.heappush(h, PQEntry(a[i]))
+#     if (i < 2):
+#       print(-1)
+#     else:
+#       max_1 = heapq.heappop(h).value
+#       max_2 = heapq.heappop(h).value
+#       max_3 = heapq.heappop(h).value
 
-      print(max_1 * max_2 * max_3)
+#       print(max_1 * max_2 * max_3)
 
-      heapq.heappush(h, PQEntry(max_1))
-      heapq.heappush(h, PQEntry(max_2))
-      heapq.heappush(h, PQEntry(max_3))
+#       heapq.heappush(h, PQEntry(max_1))
+#       heapq.heappush(h, PQEntry(max_2))
+#       heapq.heappush(h, PQEntry(max_3))
 
-# Input
+# # Input
+# n = int(input())
+# a = list(map(int, input().split()))
+
+# # Run and output
+# monk_and_multiplication(n , a)
+
+# Qheap 1
+heap = []
+item_lookup = set()
+
+def push(v):
+  heapq.heappush(heap, v)
+  item_lookup.add(v)
+
+def discard(v):
+  item_lookup.discard(v)
+
+def print_min():
+  while heap[0] not in item_lookup:
+    heapq.heappop(heap)
+
+  print(heap[0])
+
+cmds = {
+  1: push,
+  2: discard,
+  3: print_min
+}
+
 n = int(input())
-a = list(map(int, input().split()))
-
-# Run and output
-monk_and_multiplication(n , a)
+for _ in range(n):
+  data = list(map(int, input().split()))
+  cmds[data[0]](*data[1:])
