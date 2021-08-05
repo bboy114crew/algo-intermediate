058ea249e774452233b90e266ea7132fb4e250a6
Thắng Nguỵ <bboy114crew@gmail.com>
2021-08-05 23:33:56 +0700

    Solve Mice and Maze

--- hw8.py
+++ hw8.py
@@ -1,17 +1,71 @@
-# Travelling Cost
+# # Travelling Cost
+# from heapq import heappush, heappop
+# def travelling_cost(s):
+#   global graph, destinies, u, dist
+
+#   h = []
+
+#   heappush(h, (0, s))
+
+#   dist[s] = 0
+
+#   while len(h):
+#     top = heappop(h)
+#     w, b = top
+
+#     if dist[b] < w:
+#       continue
+
+#     for neighbor in graph[b]:
+#       w_n, b_n = neighbor
+#       if w_n + dist[b] < dist[b_n]:
+#         dist[b_n] = w_n + w
+#         heappush(h, (dist[b_n],  b_n))
+
+# n = int(input())
+# INF = int(1e9)
+
+# graph = [[] for index in range(505)]
+# for _ in range(n):
+#   a, b, w = list(map(int, input().split()))
+#   graph[a].append((w, b))
+#   graph[b].append((w, a))
+
+# u = int(input())
+# q = int(input())
+
+# destinies = []
+# dist = [INF for index in range(505)]
+# for _ in range(q):
+#   d = int(input())
+#   destinies.append(d)
+
+# travelling_cost(u)
+
+# for d in destinies:
+#   if dist[d] != INF:
+#     print(dist[d])
+#   else:
+#     print('NO PATH')
+
+# Mice and Maze
 from heapq import heappush, heappop
-def travelling_cost(s):
-  global graph, destinies, u, dist
+def mice_and_maze(source):
+  global e, t_max, number_of_mice_exited, graph
+
+  INF = int(1e9)  
+  dist = [INF for index in range(10005)]
+  dist[source] = 0
 
   h = []
-
-  heappush(h, (0, s))
-
-  dist[s] = 0
+  heappush(h, (0, source))
 
   while len(h):
     top = heappop(h)
     w, b = top
+
+    if b == e:
+      break
 
     if dist[b] < w:
       continue
@@ -21,29 +75,24 @@ def travelling_cost(s):
       if w_n + dist[b] < dist[b_n]:
         dist[b_n] = w_n + w
         heappush(h, (dist[b_n],  b_n))
+  
+  if dist[e] <= t_max:
+    number_of_mice_exited += 1
+
+graph = [[] for index in range(10005)]
 
 n = int(input())
-INF = int(1e9)
+e = int(input())
+t_max = int(input())
+m = int(input())
 
-graph = [[] for index in range(505)]
-for _ in range(n):
-  a, b, w = list(map(int, input().split()))
-  graph[a].append((w, b))
-  graph[b].append((w, a))
+for i in range(m):
+  a, b, t = list(map(int, input().split()))
+  graph[a].append((t, b))
 
-u = int(input())
-q = int(input())
+number_of_mice_exited = 0
 
-destinies = []
-dist = [INF for index in range(505)]
-for _ in range(q):
-  d = int(input())
-  destinies.append(d)
+for i in range(1, n + 1):
+  mice_and_maze(i)
 
-travelling_cost(u)
-
-for d in destinies:
-  if dist[d] != INF:
-    print(dist[d])
-  else:
-    print('NO PATH')
+print(number_of_mice_exited)
