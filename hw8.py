0d299151a1531144a4aa97791bf3b23369198041
Thắng Nguỵ <bboy114crew@gmail.com>
2021-08-08 11:26:47 +0700

    Solve Sending Email

--- hw8.py
+++ hw8.py
@@ -149,47 +149,93 @@
 #     the_shortest_path(cities[NAME_1], cities[NAME_2])
 #   input()
 
-# Traffic Network
+# # Traffic Network
+# from heapq import heappush, heappop
+# INF = int(1e9)  
+# MAX = 10005
+
+# def traffic_network(s, dist, graph):
+#   h = [(0, s)]
+#   dist[s] = 0
+    
+#   while len(h):
+#     w, u = heappop(h)
+      
+#     if w > dist[u]:
+#       continue
+      
+#     for weight, v in graph[u]:
+#       if w + weight < dist[v]:
+#         dist[v] = w + weight
+#         heappush(h, (dist[v], v))
+
+# t = int(input()) # the number of tests <= 20
+
+# for _ in range(t):
+#   n, m, k, s, t = list(map(int, input().split()))
+#   graph_s = [[] for _ in range(MAX)]
+#   graph_t = [[] for _ in range(MAX)]
+#   dist_t = [INF for index in range(MAX)]
+#   dist_s = [INF for index in range(MAX)]
+  
+#   # m is number of one-way roads
+#   for i in range(m):
+#     u, v, d = map(int, input().split())
+#     graph_s[u].append((d, v))
+#     graph_t[v].append((d, u))
+    
+#   traffic_network(s, dist_s, graph_s)
+#   traffic_network(t, dist_t, graph_t)
+#   res = dist_s[t]
+#   # k is number of two-way roads   
+#   for i in range(k):
+#     u, v, d = map(int, input().split())
+#     res = min(res, dist_s[u] + d + dist_t[v], dist_s[v] + d + dist_t[u])
+    
+#   print(res if res != INF else -1)
+
+# Sending Email
 from heapq import heappush, heappop
 INF = int(1e9)  
 MAX = 10005
 
-def traffic_network(s, dist, graph):
-  h = [(0, s)]
-  dist[s] = 0
-    
+def sending_email(graph, source, destination, case):
+  dist = [INF for index in range(MAX)]
+  dist[source] = 0
+
+  h = []
+  heappush(h, (0, source))
+
   while len(h):
-    w, u = heappop(h)
-      
-    if w > dist[u]:
+    top = heappop(h)
+    w, b = top
+
+    if b == destination:
+      break
+
+    if dist[b] < w:
       continue
-      
-    for weight, v in graph[u]:
-      if w + weight < dist[v]:
-        dist[v] = w + weight
-        heappush(h, (dist[v], v))
 
-t = int(input()) # the number of tests <= 20
-
-for _ in range(t):
-  n, m, k, s, t = list(map(int, input().split()))
-  graph_s = [[] for _ in range(MAX)]
-  graph_t = [[] for _ in range(MAX)]
-  dist_t = [INF for index in range(MAX)]
-  dist_s = [INF for index in range(MAX)]
+    for neighbor in graph[b]:
+      w_n, b_n = neighbor
+      if w_n + dist[b] < dist[b_n]:
+        dist[b_n] = w_n + w
+        heappush(h, (dist[b_n],  b_n))
   
-  # m is number of one-way roads
-  for i in range(m):
+  res = dist[destination]
+  if res != INF:
+    print('Case #{}: {}'.format(case, res))
+  else:
+    print('Case #{}: unreachable'.format(case))
+
+q = int(input())
+
+for i in range(q):
+  n, m, s, t = list(map(int, input().split()))
+  graph = [[] for _ in range(MAX)]
+  for _ in range(m):
     u, v, d = map(int, input().split())
-    graph_s[u].append((d, v))
-    graph_t[v].append((d, u))
-    
-  traffic_network(s, dist_s, graph_s)
-  traffic_network(t, dist_t, graph_t)
-  res = dist_s[t]
-  # k is number of two-way roads   
-  for i in range(k):
-    u, v, d = map(int, input().split())
-    res = min(res, dist_s[u] + d + dist_t[v], dist_s[v] + d + dist_t[u])
-    
-  print(res if res != INF else -1)
+    graph[u].append((d, v))
+    graph[v].append((d, u))
+  sending_email(graph, s, t, i + 1)
+
