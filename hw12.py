b5b41c0d5ebf58f85294759415bd434392108077
Thắng Nguỵ <bboy114crew@gmail.com>
2021-08-20 21:27:15 +0700

    Solve Eko

--- hw12.py
+++ hw12.py
@@ -33,30 +33,55 @@
 #     else:
 #       print('{} found at {}'.format(query, result + 1))
 
-# Pizzamania
+# # Pizzamania
+# t = int(input())
 
-t = int(input())
+# for _ in range(t):
+#   n, m = list(map(int, input().split()))
+#   array = list(map(int, input().split()))
+#   array.sort()
 
-for _ in range(t):
-  n, m = list(map(int, input().split()))
-  array = list(map(int, input().split()))
-  array.sort()
+#   start = 0
+#   tail = n - 1
+#   count = 0
 
-  start = 0
-  tail = n - 1
-  count = 0
-
-  while tail > start:
-    sum = array[tail] + array[start]
-    if sum == m:
-      count += 1
-      tail -= 1
-      start += 1
-    elif sum > m:
-      tail -= 1
-    else:
-      start += 1
+#   while tail > start:
+#     sum = array[tail] + array[start]
+#     if sum == m:
+#       count += 1
+#       tail -= 1
+#       start += 1
+#     elif sum > m:
+#       tail -= 1
+#     else:
+#       start += 1
   
-  print(count)
+#   print(count)
 
-  
+# Eko
+import math
+N, M = list(map(int, input().split()))
+
+heights_of_tree = list(map(int, input().split()))
+heights_of_tree.append(0)
+N += 1
+heights_of_tree.sort()
+
+t = 0
+sum = 0
+
+result = 0
+
+for i in range(N - 1, -1, -1):
+  t += 1
+  sum += heights_of_tree[i]
+  if sum - heights_of_tree[i] * t < M:
+    continue
+  elif sum - heights_of_tree[i] * t == M:
+    result = (sum - M) / t  
+    break
+  else:
+    result = (sum - heights_of_tree[i] - M) // (t - 1)
+    break
+
+print(math.floor(result))
