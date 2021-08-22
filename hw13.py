17c208213da3598f468637c8da1cc258358468b5
Thắng Nguỵ <bboy114crew@gmail.com>
2021-08-22 21:22:26 +0700

    Use set in Distinct Count problem

--- hw13.py
+++ hw13.py
@@ -5,15 +5,20 @@ for _ in range(T):
   N, X = list(map(int, input().split()))
   numbers = list(map(int, input().split()))
 
-  distinct = {}
+  # distinct = {}
+  # number_of_distinct = 0
+  # for num in numbers:
+  #   if distinct.get(num, None) != None:
+  #     distinct[num] += 1
+  #   else:
+  #     distinct[num] = 1
+  #     number_of_distinct += 1
 
+  distinct = set()
   for num in numbers:
-    if distinct.get(num, None) != None:
-      distinct[num] += 1
-    else:
-      distinct[num] = 1
+    distinct.add(num)
   number_of_distinct = len(distinct)
-
+  
   if number_of_distinct == X:
     print('Good')
   elif number_of_distinct < X:
