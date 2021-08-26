f93bc2750c461d0b69762396b1e387d8ed227ebb
Nguy Van Thang <nguy.van.thang@linecorp.com>
2021-08-26 13:56:40 +0700

    Solve Minimum Loss

--- hw13.py
+++ hw13.py
@@ -37,7 +37,7 @@ def insert_node(root, x):
 #   for num in numbers:
 #     distinct.add(num)
 #   number_of_distinct = len(distinct)
-  
+
 #   if number_of_distinct == X:
 #     print('Good')
 #   elif number_of_distinct < X:
@@ -45,49 +45,91 @@ def insert_node(root, x):
 #   else:
 #     print('Average')
 
-# Isenbaev's Number
-import queue
-def BFS(graph, src, rank):
-  Q = queue.Queue()
-  rank[src] = 0
-  Q.put(src)
-  while not Q.empty():
-    u = Q.get()
-    for v in graph[u]:
-      if rank[v] == 'undefined':
-        rank[v] = rank[u] + 1
-        Q.put(v)
-  return rank
+# # Isenbaev's Number
+# import queue
+# def BFS(graph, src, rank):
+#   Q = queue.Queue()
+#   rank[src] = 0
+#   Q.put(src)
+#   while not Q.empty():
+#     u = Q.get()
+#     for v in graph[u]:
+#       if rank[v] == 'undefined':
+#         rank[v] = rank[u] + 1
+#         Q.put(v)
+#   return rank
+
+# n = int(input())
+# S = dict()
+# cnt = 0
+# graph = []
+
+# # Lưu Tên map với số sau đó lưu vào BTS self-balance để search cho tối ưu
+
+# for i in range(n):
+#   line = input().split()
+#   v = []
+#   for name in line:
+#     if name in S:
+#       id = S[name]
+#     else:
+#       S[name] = cnt
+#       id = cnt
+#       graph.append([])
+#       cnt+= 1
+#     v.append(id)
+#   for x in v:
+#     for y in v:
+#       if x != y:
+#         graph[x].append(y)
+# rank = ['undefined' for i in range(cnt)]
+# if 'Isenbaev' in S:
+#   rank = BFS(graph, S['Isenbaev'], rank)
+# a = []
+# for name in S:
+#   a.append(name)
+# a.sort()
+# for name in a:
+#   print(name + ' ' + str(rank[S[name]]))
+
+# Minimum Loss
+'''
+Ý tưởng
+Giả sử ta bán nhà vào năm thứ 𝑖, khi đó cần chọn năm thứ 𝑥 1 ≤ 𝑥 < 𝑖 để
+mua nhà sao cho 𝑝𝑥 > 𝑝𝑖 và 𝑝𝑥 nhỏ nhất có thể.
+Sau đó cập nhật lại kết quả: 𝑎𝑛𝑠 = min 𝑎𝑛𝑠, 𝑝𝑥 − 𝑝𝑖
+.
+Để tìm 𝑝𝑥 một cách hiệu quả:
+• Ta đưa các giá trị 𝑝1, 𝑝2, … , 𝑝𝑖−1 vào một Binary Search Tree.
+• Dùng hàm “upper_bound” để tìm giá trị nhỏ nhất lớn hơn 𝑝𝑖
+.
+Python không có sẵn BST trong thư viện, nên ta phải tự cài self-balancing
+BST để tối ưu thời gian chạy.
+'''
+INF = int(1e20)
+def upper_bound(root, x):
+  if root == None:
+    return root
+  if x >= root.key:
+    return upper_bound(root.right, x)
+  ub_left = upper_bound(root.left, x)
+  if ub_left != None:
+    return ub_left
+  else:
+    return root
 
 n = int(input())
-S = dict()
-cnt = 0
-graph = []
+prices = list(map(int, input().split()))
 
-# Lưu Tên map với số sau đó lưu vào BTS self-balance để search cho tối ưu
+root = None
+ans = INF
 
 for i in range(n):
-  line = input().split()
-  v = []
-  for name in line:
-    if name in S:
-      id = S[name]
-    else:
-      S[name] = cnt
-      id = cnt
-      graph.append([])
-      cnt+= 1
-    v.append(id)
-  for x in v:
-    for y in v:
-      if x != y:
-        graph[x].append(y)
-rank = ['undefined' for i in range(cnt)]
-if 'Isenbaev' in S:
-  rank = BFS(graph, S['Isenbaev'], rank)
-a = []
-for name in S:
-  a.append(name)
-a.sort()
-for name in a:
-  print(name + ' ' + str(rank[S[name]]))
+  ub_node = upper_bound(root, prices[i])
+  if ub_node != None:
+    ans = min(ans, ub_node.key - prices[i])
+  root = insert_node(root, prices[i])
+print(ans)
+
+
+
