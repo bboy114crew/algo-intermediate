a6b5d801e2707ce7d1b5841e07c9ad81f7501020
Thắng Nguỵ <bboy114crew@gmail.com>
2021-09-05 23:32:39 +0700

    Solve Ice Skating use union by rank

--- hw16.py
+++ hw16.py
@@ -193,3 +193,64 @@ uniset(u, v) Hợp tập hợp chứa u và tập hợp chứa v thành 1 tập 
 
 #   if Q > 0:
 #     print()
+
+# Ice Skating
+parent = []
+ranks = []
+
+def make_set(n):
+  global parent, ranks
+  parent = [i for i in range(n)]
+  ranks = [i for i in range(n)]
+
+def find_set(u):
+  global parent
+  if parent[u] != u:
+    parent[u] = find_set(parent[u])
+  return parent[u]
+
+def union_set(u, v):
+  global parent, ranks
+  up = find_set(u)
+  vp = find_set(v)
+
+  if up == vp:
+    return
+
+  if ranks[up] > ranks[vp]:
+    parent[vp] = up
+  elif ranks[up] < ranks[vp]:
+    parent[up] = vp
+  else:
+    parent[up] = vp
+    ranks[vp] += 1
+
+n = int(input())
+
+x = [0 for _ in range(n)]
+y = [0 for _ in range(n)]
+
+for i in range(n):
+  px, py = list(map(int, input().split()))
+  x[i] = px
+  y[i] = py
+
+make_set(n)
+
+for i in range(n):
+  for j in range(i + 1, n):
+    if x[i] == x[j] or y[i] == y[j]:
+      u = find_set(i)
+      v = find_set(j)
+
+      if u != v:
+        union_set(u, v)
+
+ans = 0
+
+for i in range(n):
+  if parent[i] == i:
+    ans += 1
+
+ans -= 1
+print(ans)
