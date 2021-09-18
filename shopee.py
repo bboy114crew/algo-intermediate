95075ac637998c039fceb1b5a0ce3c94823031af
Thắng Nguỵ <bboy114crew@gmail.com>
2021-09-18 18:09:48 +0700

    Update shopee question

--- shopee.py
+++ shopee.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/candy/
 def candy(ratings):
   number_of_ratings = len(ratings)
-  result = [1 for i in range(number_of_ratings)]
+  result = [1 for _ in range(number_of_ratings)]
 
   for idx in range(number_of_ratings - 1):
     if ratings[idx] < ratings[idx + 1]:
@@ -13,4 +13,4 @@ def candy(ratings):
 
   return sum(result)
 
-print(candy([1,2, 87, 87, 87, 2, 1]))
+print(candy([1, 2, 87, 87, 87, 2, 1]))
